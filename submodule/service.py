ead173671c14b406921c9957f5d2210d31fa2fad
argonaultes <gael@argonaultes.fr>
2025-04-14 17:51:26 +0200

    client asked for it

--- submodule/service.py
+++ submodule/service.py
@@ -9,7 +9,7 @@ REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing requ
 @REQUEST_TIME.time()
 def process_request(t):
     """A dummy function that takes some time."""
-    time.sleep(t * 2)
+    time.sleep(t * 2 * 100)
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
